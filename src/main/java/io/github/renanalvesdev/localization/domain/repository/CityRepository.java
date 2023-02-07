9d6869e641be51ab8de972153828e16d42a2b2fe
Renan Alves <renanalvesemail@gmail.com>
2023-02-07 23:34:55 +0000

    adding query methods with String variants

--- src/main/java/io/github/renanalvesdev/localization/domain/repository/CityRepository.java
+++ src/main/java/io/github/renanalvesdev/localization/domain/repository/CityRepository.java
@@ -1,9 +1,26 @@
 package io.github.renanalvesdev.localization.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.github.renanalvesdev.localization.domain.entity.City;
 
 public interface CityRepository extends JpaRepository<City, Long>{
 
+	
+	List<City> findByName(String name);
+	
+	//searching for city having name starting with requested string
+	List<City> findByNameStartingWith(String name);
+	
+	//searching for city having name ending with requested string
+	List<City> findByNameEndingWith(String name);
+	
+	// searching for city having name containing requested string
+	List<City> findByNameContaining(String name);
+	
+	List<City> findByHabitants(Long habitants);
+
+	
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 create table tb_city (
 	id_city bigint not null primary key ,
 	name varchar(50) not null,
-	habitants bigint
+	qtd_habitants bigint
 );
 
 insert into tb_city
-(id_city, name, habitants)
+(id_city, name, qtd_habitants)
 values 
 	(1, 'Lisbon', 500000),
 	(2, 'Porto', 214000);
