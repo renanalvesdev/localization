91386d38ebc0d43fd0ff3917f500c4205eb11023
Renan Alves <renanalvesemail@gmail.com>
2023-02-27 23:30:12 +0000

    using native SQL and projections

--- src/main/java/io/github/renanalvesdev/localization/domain/repository/CityRepository.java
+++ src/main/java/io/github/renanalvesdev/localization/domain/repository/CityRepository.java
@@ -8,11 +8,26 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import io.github.renanalvesdev.localization.domain.entity.City;
+import io.github.renanalvesdev.localization.domain.repository.projections.CityProjection;
 
 public interface CityRepository extends JpaRepository<City, Long>, JpaSpecificationExecutor<City>{
 
+	
+	//if we put * or some property using the correct return, everything works fine
+	//but if we put more than one property with entity as list return, will break
+	//to solve this, we can use Projections.
+	@Query(nativeQuery =true, value="select c.name from tb_city as c where c.name =:name")
+	List<String> findByNameNativeSQL(@Param("name") String name);
+	
+	
+	//wont work because projection doesnt have id_city alias.
+	//instead , we only have id. so we should apply an alias to id property
+	@Query(nativeQuery =true, value="select c.id_city as id, c.name from tb_city as c where c.name =:name")
+	List<CityProjection> findByNameNativeSQLProjection(@Param("name") String name);
+	
 	
 	List<City> findByName(String name);
 	
---
+++ src/main/java/io/github/renanalvesdev/localization/domain/repository/projections/CityProjection.java
@@ -0,0 +1,8 @@
+package io.github.renanalvesdev.localization.domain.repository.projections;
+
+public interface CityProjection {
+
+	
+	Long getId();
+	String getName();
+}
